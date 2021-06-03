6abdcbf4caee372f5b05961f911028363c93f94c
taigatomita <tiger101837@gmail.com>
2021-06-03 19:47:51 +0900

    add fizubuzz

---
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -0,0 +1,12 @@
+x = 1..20
+x.each do |i|
+    if i % 5 == 0 && i % 3 == 0
+        puts "FizzBuzz"
+    elsif i % 3 == 0
+        puts "Fizz"
+    elsif i % 5 == 0
+        puts "Buzz"
+    else
+        puts i
+    end
+end
