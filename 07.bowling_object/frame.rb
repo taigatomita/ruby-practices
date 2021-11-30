b9e9664c228cf6e4f5dc641ebb0a170bf333781d
taigatomita <tiger101837@gmail.com>
2021-11-30 23:49:18 +0900

    ボウリングのスコア計算プログラム(OOP)を追加

---
+++ 07.bowling_object/frame.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'shot'
+
+class Frame
+  attr_reader :first_shot
+
+  def initialize(marks)
+    @first_shot = Shot.new(marks[0])
+    @second_shot = marks[1] ? Shot.new(marks[1]) : Shot.new(0)
+    @third_mark = marks[2] ? Shot.new(marks[2]) : Shot.new(0)
+  end
+
+  def strike?
+    first_shot.score == 10
+  end
+
+  def spare?
+    !strike? && total_of_2_throws == 10
+  end
+
+  def sum_scores
+    [@first_shot.score, @second_shot.score, @third_mark.score].sum
+  end
+
+  def total_of_2_throws
+    [@first_shot.score, @second_shot.score].sum
+  end
+end
---
+++ 07.bowling_object/game.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require_relative 'frame'
+require_relative 'shot'
+
+class Game
+  def initialize(argv)
+    @scores = argv
+  end
+
+  def main
+    array_of_scores = insert_0_after_x(@scores.split(','))
+    devided_scores = slice_into_frames(array_of_scores)
+    frames = adjusting_the_final_frame(devided_scores).map { |f| Frame.new(f) }
+    p score_calculation(frames)
+  end
+
+  def insert_0_after_x(scores)
+    shots = []
+    scores.each { |s| s == 'X' ? shots.push(s, '0') : shots.push(s) }
+    shots
+  end
+
+  def adjusting_the_final_frame(scores)
+    last_shots = scores.slice!(9..).flatten.reject { |s| s == '0' }
+    scores << last_shots
+    scores
+  end
+
+  def slice_into_frames(array_of_socres)
+    array_of_socres.each_slice(2).to_a
+  end
+
+  def strike_score_calculation(frames, idx)
+    point = frames[idx + 1].total_of_2_throws
+    point += frames[idx + 2].first_shot.score if frames[idx + 1].strike? && frames[idx + 2]
+    point
+  end
+
+  def score_calculation(frames)
+    point = 0
+    frames.each_with_index do |frame, idx|
+      point += frame.sum_scores
+      next unless frames[idx + 1]
+
+      if frame.strike?
+        point += strike_score_calculation(frames, idx)
+      elsif frame.spare?
+        point += frames[idx + 1].first_shot.score.to_i
+      end
+    end
+    point
+  end
+end
+
+game_score = Game.new(ARGV[0])
+game_score.main
